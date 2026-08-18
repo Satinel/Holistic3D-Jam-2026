c6023ff45b15369297f00726d3d040145697bd6d
Nimufu <angelonmyknees@gmail.com>
2026-08-18 12:25:09 -0300

    EnemySpawner script and prefab with empty Enemy class

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public event System.Action OnFinalWaveSpawned;
+
+    [System.Serializable] class Wave
+    {
+        public Enemy[] Enemies;
+    }
+
+    [SerializeField] Wave[] _waves;
+    [SerializeField] Transform[] _spawnPoints;
+    [SerializeField] float _minSpawnTime = 0.35f, _maxSpawnTime = 0.85f;
+
+    int _waveIndex, _enemyIndex;
+    float _spawnTimer = 1f;
+    bool _isSpawning, _isFinished;
+
+    void Update()
+    {
+        if(!_isSpawning || _isFinished) { return; }
+        if(_waveIndex >= _waves.Length) { return; }
+
+        if(_spawnTimer > 0)
+        {
+            _spawnTimer -= Time.deltaTime;
+            return;
+        }
+
+        if(_waves[_waveIndex].Enemies.Length < _enemyIndex)
+        {
+            Enemy enemy = Instantiate(_waves[_waveIndex].Enemies[_enemyIndex], _spawnPoints[Random.Range(0, _spawnPoints.Length)].position, Quaternion.identity, transform);
+            // TODO : Initialize enemy
+
+            _enemyIndex++;
+
+            if(_waves[_waveIndex].Enemies.Length >= _enemyIndex)
+            {
+                _isSpawning = false;
+                _enemyIndex = 0;
+                _waveIndex++;
+
+                if(_waveIndex >= _waves.Length)
+                {
+                    _isFinished = true;
+                    OnFinalWaveSpawned?.Invoke();
+                }
+            }
+            else
+            {
+                _spawnTimer += Random.Range(_minSpawnTime, _maxSpawnTime);
+            }
+        }
+    }
+}
