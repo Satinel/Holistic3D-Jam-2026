a2429c7ee105eb8de693b090b6174108d10cb733
Nimufu <angelonmyknees@gmail.com>
2026-09-06 01:58:16 -0300

    Added some footstep sounds which maybe don't sound all that great

--- Assets/Scripts/Player/PlayerAnimationEvents.cs
+++ Assets/Scripts/Player/PlayerAnimationEvents.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class PlayerAnimationEvents : MonoBehaviour
 {
     [SerializeField] PlayerController _playerController;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip[] _footstepsSFX;
+    [SerializeField] float _footstepVolume = 0.5f;
 
     void AttackAnimationEvent()
     {
         _playerController.Attack();
+    }
+
+    void FootstepAnimationEvent()
+    {
+        _audioSource.PlayOneShot(_footstepsSFX[Random.Range(0, _footstepsSFX.Length)], _footstepVolume);
     }
 
     void DeathAnimationEvent()
