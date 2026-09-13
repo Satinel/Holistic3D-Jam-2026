605d1e32462f794a724e8f75402e4a6f54c6b159
Nimufu <angelonmyknees@gmail.com>
2026-09-13 16:59:19 -0300

    Navigation for instantiated RebindActionUI game objects & Working rebinding (and resetting) of UI/Navigation input

--- Assets/Scripts/UI/KeyRebinding.cs
+++ Assets/Scripts/UI/KeyRebinding.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class KeyRebinding : MonoBehaviour
 {
     [SerializeField] InputActionAsset _inputActions;
     [SerializeField] Canvas _rebindCanvas;
     [SerializeField] RebindActionUI _rebindActionUIPrefab;
-    [SerializeField] GameObject _keyboardUI, _gamepadUI, _keyboadMainMenuButton, _gamepadMainMenuButton;
+    [SerializeField] GameObject _keyboardUI, _gamepadUI;
+    [SerializeField] Button _keyboadMainMenuButton, _gamepadMainMenuButton;
     [SerializeField] Transform _keyboardBindingsParent, _gamepadBindingsParent;
     [SerializeField] GameObject _keyboardPrompt, _gamepadPrompt;
 
     readonly List<RebindActionUI> _instantiatedKeyboardRebinds = new(), _instantiatedGamepadRebinds = new();
     InputActionRebindingExtensions.RebindingOperation _rebindOperation;
+    InputAction _navigateAction;
 
     static readonly string OVERRIDES_STRING = "BindingOverrides";
 
     void Awake()
     {
+        _navigateAction = _inputActions.FindAction("UI/Navigate");
+
         LoadOverrides();
+
         InputManager.ReportMoveAction += InputManager_ReportMoveAction;
         InputManager.ReportRebindableActions += InputManager_ReportRebindableActions;
         InputManager.OnOptionsPressed += InputManager_OnOptionsPressed;
@@ -31,6 +37,7 @@ public class KeyRebinding : MonoBehaviour
         InputManager.ReportMoveAction -= InputManager_ReportMoveAction;
         InputManager.ReportRebindableActions -= InputManager_ReportRebindableActions;
         InputManager.OnOptionsPressed -= InputManager_OnOptionsPressed;
+
         _rebindOperation?.Cancel();
         _rebindOperation?.Dispose();
         _rebindOperation = null;
@@ -41,25 +48,51 @@ public class KeyRebinding : MonoBehaviour
         for(int i = 1; i < 5; i++)
         {
             RebindActionUI rebindUI = Instantiate(_rebindActionUIPrefab, _keyboardBindingsParent);
-            rebindUI.Initialize(this, moveAction, i, false);
+            rebindUI.Initialize(this, moveAction, i, false, true);
             _instantiatedKeyboardRebinds.Add(rebindUI);
         }
     }
 
     void InputManager_ReportRebindableActions(InputAction[] rebindableActions)
     {
-        for(int i = 0; i < rebindableActions.Length; i++)
+        for (int i = 0; i < rebindableActions.Length; i++)
         {
             RebindActionUI keyboardRebindUI = Instantiate(_rebindActionUIPrefab, _keyboardBindingsParent);
             keyboardRebindUI.Initialize(this, rebindableActions[i], 0, false);
             _instantiatedKeyboardRebinds.Add(keyboardRebindUI);
-            // TODO : Set navigation of buttons
 
             RebindActionUI gamepadRebindUI = Instantiate(_rebindActionUIPrefab, _gamepadBindingsParent);
             gamepadRebindUI.Initialize(this, rebindableActions[i], 1, true);
             _instantiatedGamepadRebinds.Add(gamepadRebindUI);
-            // TODO : Set navigation of buttons
         }
+
+        SetNavigations(_instantiatedKeyboardRebinds, _keyboadMainMenuButton);
+        SetNavigations(_instantiatedGamepadRebinds, _gamepadMainMenuButton);
+    }
+
+    void SetNavigations(List<RebindActionUI> actionUIList, Button mainMenuButton)
+    {
+        for(int i = 0; i < actionUIList.Count; i++)
+        {
+            if(i == 0)
+            {
+                actionUIList[i].SetFirstButtonNavigation(mainMenuButton, actionUIList[i + 1]);
+            }
+            else if(i < actionUIList.Count - 1)
+            {
+                actionUIList[i].SetButtonNavigation(actionUIList[i - 1], actionUIList[i + 1]);
+            }
+            else
+            {
+                actionUIList[i].SetLastButtonNavigation(actionUIList[i - 1], mainMenuButton);
+            }
+        }
+
+        Navigation navigation = mainMenuButton.navigation;
+        navigation.mode = Navigation.Mode.Explicit;
+        navigation.selectOnDown = actionUIList[0].RebindB;
+        navigation.selectOnUp = actionUIList[^1].RebindB;
+        mainMenuButton.navigation = navigation;
     }
 
     void InputManager_OnOptionsPressed()
@@ -102,6 +135,12 @@ public class KeyRebinding : MonoBehaviour
                                 });
 
         _rebindOperation.Start();
+    }
+
+    public void RebindNavigate(int bindingIndex, string bindingPath)
+    {
+        _navigateAction.ApplyBindingOverride(bindingIndex, bindingPath);
+        SaveOverrides();
     }
 
     public void GamepadButtonRebindAction(InputAction inputAction, int bindingIndex, Action onComplete)
@@ -175,14 +214,14 @@ public class KeyRebinding : MonoBehaviour
             _keyboardUI.SetActive(false);
             _gamepadUI.SetActive(true);
             EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(_gamepadMainMenuButton);
+            EventSystem.current.SetSelectedGameObject(_gamepadMainMenuButton.gameObject);
         }
         else
         {
             _gamepadUI.SetActive(false);
             _keyboardUI.SetActive(true);
             EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(_keyboadMainMenuButton);
+            EventSystem.current.SetSelectedGameObject(_keyboadMainMenuButton.gameObject);
         }
     }
 
@@ -192,12 +231,12 @@ public class KeyRebinding : MonoBehaviour
         if(_keyboardUI.activeSelf)
         {
             EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(_keyboadMainMenuButton);
+            EventSystem.current.SetSelectedGameObject(_keyboadMainMenuButton.gameObject);
         }
         else
         {
             EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(_gamepadMainMenuButton);
+            EventSystem.current.SetSelectedGameObject(_gamepadMainMenuButton.gameObject);
         }
     }
 
--- Assets/Scripts/UI/RebindActionUI.cs
+++ Assets/Scripts/UI/RebindActionUI.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using TMPro;
 
 public class RebindActionUI : MonoBehaviour
 {
+    [field:SerializeField] public Button RebindB { get; private set; }
+    [field:SerializeField] public Button DefaultB { get; private set; }
     [SerializeField] TextMeshProUGUI _actionNameText, _rebindButtonText, _defautButtonText;
-    [SerializeField] int _index;
-    [SerializeField] bool _isGamepadBinding;
+    int _index;
+    bool _isGamepadBinding, _isMoveBinding;
     KeyRebinding _keyRebinding;
     InputAction _inputAction;
     bool _isDestroyed;
@@ -16,40 +19,91 @@ public class RebindActionUI : MonoBehaviour
         _isDestroyed = true;    // Alledgedly this can be read after being set true
     }
 
-    public void Initialize(KeyRebinding keyRebinding, InputAction inputAction, int index, bool isGamepadBinding)
+    public void Initialize(KeyRebinding keyRebinding, InputAction inputAction, int index, bool isGamepadBinding, bool isMoveBinding = false)
     {
         _keyRebinding = keyRebinding;
         _inputAction = inputAction;
         _index = index;
         _isGamepadBinding = isGamepadBinding;
+        _isMoveBinding = isMoveBinding;
 
         _actionNameText.text = string.IsNullOrWhiteSpace(_inputAction.bindings[index].name) ? _inputAction.name : _inputAction.bindings[_index].name;
+        _rebindButtonText.text = _inputAction.GetBindingDisplayString(_index, InputBinding.DisplayStringOptions.DontUseShortDisplayNames);
         _defautButtonText.text = $"Restore Default\n[{InputControlPath.ToHumanReadableString(_inputAction.bindings[_index].path, InputControlPath.HumanReadableStringOptions.OmitDevice)}]";
-        SetRebindButtonText();
     }
 
-    void SetRebindButtonText()
+    public void SetFirstButtonNavigation(Button button, RebindActionUI next)
+    {
+        Navigation rebindNavigation = RebindB.navigation;
+        rebindNavigation.mode = Navigation.Mode.Explicit;
+        rebindNavigation.selectOnUp = button;
+        rebindNavigation.selectOnDown = next.RebindB;
+        RebindB.navigation = rebindNavigation;
+
+        Navigation defaultNavigation = DefaultB.navigation;
+        defaultNavigation.mode = Navigation.Mode.Explicit;
+        defaultNavigation.selectOnUp = button;
+        defaultNavigation.selectOnDown = next.DefaultB;
+        DefaultB.navigation = defaultNavigation;
+    }
+
+    public void SetButtonNavigation(RebindActionUI previous, RebindActionUI next)
+    {
+        Navigation rebindNavigation = RebindB.navigation;
+        rebindNavigation.mode = Navigation.Mode.Explicit;
+        rebindNavigation.selectOnUp = previous.RebindB;
+        rebindNavigation.selectOnDown = next.RebindB;
+        RebindB.navigation = rebindNavigation;
+
+        Navigation defaultNavigation = DefaultB.navigation;
+        defaultNavigation.mode = Navigation.Mode.Explicit;
+        defaultNavigation.selectOnUp = previous.DefaultB;
+        defaultNavigation.selectOnDown = next.DefaultB;
+        DefaultB.navigation = defaultNavigation;
+    }
+
+    public void SetLastButtonNavigation(RebindActionUI previous, Button button)
+    {
+        Navigation rebindNavigation = RebindB.navigation;
+        rebindNavigation.mode = Navigation.Mode.Explicit;
+        rebindNavigation.selectOnUp = previous.RebindB;
+        rebindNavigation.selectOnDown = button;
+        RebindB.navigation = rebindNavigation;
+
+        Navigation defaultNavigation = DefaultB.navigation;
+        defaultNavigation.mode = Navigation.Mode.Explicit;
+        defaultNavigation.selectOnUp = previous.DefaultB;
+        defaultNavigation.selectOnDown = button;
+        DefaultB.navigation = defaultNavigation;
+    }
+
+    void RebindCompleteCallback()
     {
         if(_isDestroyed) { return; }    // Probably totally unnecessary safeguard that would never come up (and does it really work?)
 
         _rebindButtonText.text = _inputAction.GetBindingDisplayString(_index, InputBinding.DisplayStringOptions.DontUseShortDisplayNames);
+
+        if(_isMoveBinding)
+        {
+            _keyRebinding.RebindNavigate(_index, _inputAction.bindings[_index].effectivePath);
+        }
     }
 
     public void RebindButton()
     {
         if(_isGamepadBinding)
         {
-            _keyRebinding.GamepadButtonRebindAction(_inputAction, _index, SetRebindButtonText);
+            _keyRebinding.GamepadButtonRebindAction(_inputAction, _index, RebindCompleteCallback);
         }
         else
         {
-            _keyRebinding.KeyboardButtonRebindAction(_inputAction, _index, SetRebindButtonText);
+            _keyRebinding.KeyboardButtonRebindAction(_inputAction, _index, RebindCompleteCallback);
         }
     }
 
     public void DefaultButton()
     {
         _keyRebinding.ResetBinding(_inputAction, _index);
-        SetRebindButtonText();
+        RebindCompleteCallback();
     }
 }
